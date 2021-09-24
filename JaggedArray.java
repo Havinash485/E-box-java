709e058197cd97bb9504941c968adae9ed3ee766
Havinash485 <91141017+Havinash485@users.noreply.github.com>
2021-09-24 19:57:15 +0530

    Add files via upload

---
+++ JaggedArray.java
@@ -0,0 +1,31 @@
+package fun;
+
+public class JaggedArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr=new int[3][];
+		arr[0]=new int[3];
+		arr[1]=new int[4];
+		arr[2]=new int[2];
+		int value=10;
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				arr[i][j]=value++;
+			}
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+			   System.out.print(arr[i][j]+" ");	
+			}
+			System.out.println();
+		}
+
+	}
+
+}
