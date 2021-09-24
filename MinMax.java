7127b98a650163470fde61760d3c966bd89c38b5
Havinash485 <91141017+Havinash485@users.noreply.github.com>
2021-09-24 20:10:32 +0530

    Update MinMax.java

--- MinMax.java
+++ MinMax.java
@@ -14,7 +14,7 @@ public class MinMax {
 		System.out.println("min value is "+min);
 
 	}
-	public static int minmax(int num1,int num2)//formal parameters//function definition
+	public static int max(int num1,int num2)//formal parameters//function definition
 	{
 		if(num1>num2)
 		{
@@ -25,5 +25,17 @@ public class MinMax {
 			return num2;
 		}
 	}
+	public static int min(int num1,int num2)//formal parameters//function definition
+	{
+		if(num1>num2)
+		{
+			return num1;
+		}
+		else
+		{
+			return num2;
+		}
+	}
+
 
 }
