cb1210c83019fd27d50f1ba8247ba338ac03add6
Havinash485 <91141017+Havinash485@users.noreply.github.com>
2021-10-01 09:28:54 +0530

    Add files via upload

---
+++ LargestPalindrome.java
@@ -0,0 +1,32 @@
+package fun;
+
+public class LargestPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int max=0;
+		for(int i=10;i<=99;i++)
+		{
+			for(int j=10;j<=99;j++)
+			{
+				if((i*j>max)&&(i*j==reverse(i*j)))
+				{
+					max=i*j;
+				}
+			}
+		}
+      System.out.println(max);
+	}
+	public static int reverse(int n)
+	{
+		int rem=0,rev=0;
+		while(n>1)
+		{
+			rem=n%10;
+			rev=rev*10+rem;
+			n=n/10;
+		}
+		return rev;
+	}
+
+}
